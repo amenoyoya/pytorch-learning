a29b0e9157e0c5cae8590ccc2ba033da3bb927ba
amenoyoya <mtyswe3@gmail.com>
2019-08-18 14:12:11 +0900

    update: TorchVision library

--- lib/TorchVision.jl
+++ lib/TorchVision.jl
@@ -26,16 +26,36 @@ SOFTWARE.
 
 module TorchVision
     export torch, torchvision, transforms, models
-    export predict
-    export make_transformer
-    export make_transformer_for_vgg16
+    export Color, Train, Valid, Phase
+    export seed_random!, predict
+    export make_transformer, make_transformer_for_vgg16, make_transformer_for_training, make_transformer_for_vgg16_training
 
-    using PyCall
+    using PyCall, Random
 
     const torch = pyimport("torch")
     const torchvision = pyimport("torchvision")
     const transforms = torchvision.transforms
     const models = torchvision.models
+
+    # 色表現型
+    const Color = Tuple{AbstractFloat,AbstractFloat,AbstractFloat}
+
+    # 機械学習モード表現型
+    abstract type Train end
+    abstract type Valid end
+    const Phase = Union{Type{Train}, Type{Valid}}
+
+    # 型（CamelCase）を文字列（snake_case）に変換する
+    macro typestr(T)
+        replace(replace(string(T), r"([A-Z])" => c -> "_" * lowercase(c)), r"^_" => s"")
+    end
+
+    # 乱数初期化
+    ## 値を破壊的に変更する（もしくはグローバル変数の状態に影響を与える）関数には慣例的に`!`をつける
+    seed_random!(seed::Int) = begin
+        Random.seed!(seed)
+        torch.manual_seed(seed) # PyTorchの乱数初期化
+    end
 
     # torch.Modelに予測させる
     ## Julia標準のArrayデータを入力として受け取り、Arrayで返す
@@ -57,6 +77,7 @@ module TorchVision
     end
 
     # PIL.ImageオブジェクトをVGG16ニューラルネットワークモデル用のデータ形式に変換する関数を生成
+    # () -> ((img::PyObject) -> Array{Float32,3})
     make_transformer_for_vgg16() = begin
         transformer = make_transformer(
             transforms.Resize(224), # 短い方の辺の長さが224になるようにリサイズ
@@ -70,5 +91,66 @@ module TorchVision
         ## clamp(x, low, high): if x < low then x = low, if x > high then x = high
         ## `.`演算子: 関数を行列に対して適用させる
         return (image::PyObject) -> clamp.(transformer(image), 0, 1)
+    end
+
+    # モデル訓練用に入力画像を変換する関数を生成
+    # (resize, std, mean) -> ((img::PyObject, phase::Phase) -> Array{Float32,3})
+    ## @param resize::Int = リサイズする大きさ
+    ## @param std::Color = 各色チャンネルの平均値
+    ## @param mean::Color = 各色チャンネルの標準偏差
+    make_transformer_for_training(resize::Int, std::Color, mean::Color) = begin
+        trasformers = Dict(
+            "train" => make_transformer(
+                transforms.RandomResizedCrop(resize; scale=(0.5, 1.0)),
+                transforms.RandomHorizontalFlip(),
+                transforms.Normalize(mean, std)
+            ),
+            "valid" => make_transformer(
+                transforms.Resize(resize),
+                transforms.CenterCrop(resize),
+                transforms.Normalize(mean, std)
+            )
+        )
+        return (img::PyObject, phase::Phase) -> trasformers[@typestr phase](img)
+    end
+
+    # VGG-16モデル訓練用に入力画像を変換する関数を生成
+    # (resize, std, mean) -> ((img::PyObject, phase::Phase) -> Array{Float32,3})
+    make_transformer_for_vgg16_training() = begin
+        transformer = make_transformer_for_training(224, (0.485, 0.456, 0.406), (0.229, 0.224, 0.225))
+        return (img::PyObject, phase::Phase) -> clamp.(transformer(img, phase), 0, 1)
+    end
+
+    # PyTorch規格の画像データセット型生成マクロ
+    # 要 PyCall, ./Image
+    # @param TypeName: 型名
+    # @param make_dataset_list_function(phase::Phase)::Array{String,1}: データセットのファイルパスリストを返す関数
+    # @param image_transform_function(img::PyObject, phase::Phase)::Array{Float32,3}: 画像をモデル入力用に変換する関数
+    # @param labeling_function(img_path::String)::Int: データセットのラベルインデックスを返す関数
+    ## esc(SymbolExpression) を使うことで マクロ実行位置にそのままコードを埋め込むことができる
+    ## ※ そのまま埋め込まれるため変数スコープ等は効かない
+    macro image_dataset(TypeName, make_dataset_list_function, image_transform_function, labeling_function)
+        esc(quote
+            @pydef mutable struct $TypeName <: TorchVision.torch.utils.data.Dataset
+                __init__(self, phase::TorchVision.Phase) = begin
+                    pybuiltin(:super)($TypeName, self).__init__()
+                    self.phase = phase
+                    self.file_list = $make_dataset_list_function(phase)
+                end
+                
+                __len__(self) = length(self.file_list)
+                
+                __getitem__(self, index::Int) = begin
+                    # index番目の画像をロード
+                    ## Juliaのindexは1〜なので +1 する
+                    img_path = self.file_list[index + 1]
+                    img = Image.open(img_path).convert("RGB") # グレースケール画像は強制的にRGB画像に変換
+                    img_transformed = $image_transform_function(img, self.phase)
+                    # ラベリング
+                    label = $labeling_function(img_path)
+                    return img_transformed, label
+                end
+            end
+        end)
     end
 end
